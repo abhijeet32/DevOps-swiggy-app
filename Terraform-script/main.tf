de1fe3d22fefafa734f8d4d7b9f450267970176e
abhijeet32 <kumarabhijeetips@gmail.com>
2024-10-25 17:56:12 +0530

    Swiggy app

---
+++ Terraform-script/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "Project-SG" {
+    name = "Project-SG"
+    description = "Open 22,443,80,8080,9000"
+
+    # Define a single ingress rule 
+    ingress =[
+        for port in [22, 80, 443, 8080, 9000, 3000] : {
+            description      = "TLS from VPC"
+            from_port        = port
+            to_port          = port
+            protocol         = "tcp"
+            cidr_blocks      = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            security_groups  = []
+            self             = false
+        }
+    ]
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "Project-SG"
+    }
+}
+
+resource "aws_instance" "web" {
+    ami                    = "ami-0866a3c8686eaeeba"
+    instance_type          = "t2.large"
+    key_name               = "demo_12"
+    vpc_security_group_ids = [aws_security_group.Project-SG.id]
+    user_data              = templatefile("./resource.sh", {})
+
+    tags = {
+        Name = "Project-SG"
+    }
+    root_block_device {
+        volume_size = 30
+    }
+}
