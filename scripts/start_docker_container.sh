c52ac4a9983ad6ba224860810412e3d84f20e35e
Abhijeet kumar <150456058+abhijeet32@users.noreply.github.com>
2025-01-18 20:21:09 +0530

    updating start container.sh

--- scripts/start_docker_container.sh
+++ scripts/start_docker_container.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+set -e
 
 # Pull the latest Docker image
-docker pull $IMAGE_NAME
-
-# Stop and remove any existing container with the same name
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+docker pull abhi0874/swiggy-clone-app
 
 # Start the container
-docker run -d -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -p 3000:3000 abhi0874/swiggy-clone-app
